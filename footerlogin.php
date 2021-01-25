0015edd15875909247b99e9a552eddf658396e85
nictreybig <77948871+nictreybig@users.noreply.github.com>
2021-01-24 22:31:43 -0600

    Create footerlogin.php

---
+++ footerlogin.php
@@ -0,0 +1,87 @@
+<!-- Start of Footer -->
+<div id="Dotyouri">
+<svg>
+  <circle cx="3" cy="3" r="3" stroke=null stroke-width="3" fill="red" />
+</svg>
+</div>
+<div id="Dotyourii">
+<svg>
+  <circle cx="1" cy="1" r="1" stroke=null stroke-width="1" fill="black" />
+</svg>
+</div>
+<div class="leaf5">
+  <svg xmlns="http://www.w3.org/2000/svg" style="vector-effect: non-scaling-stroke;" stroke="null">
+    <g stroke="null">
+      <path stroke="#f90202" d="m70,159c102,-16.597 98.862,-36 138,-35c39.138,-2 49,19.403 134,35c-2,-0.403 -62.862,0 -138,0c-72.138,-1 -135,0.597 -134,0z" id="svg_10" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="0" fill="#e3ffe3"
+      />
+    </g>
+  </svg>
+</div>
+<div class="leaf6">
+  <svg xmlns="http://www.w3.org/2000/svg" style="vector-effect: non-scaling-stroke;" stroke="null">
+    <g stroke="null">
+      <path stroke="#f90202" d="m70,159c102,-16.597 98.862,-36 138,-35c39.138,-2 49,19.403 134,35c-2,-0.403 -62.862,0 -138,0c-72.138,-1 -135,0.597 -134,0z" id="svg_10" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="0" fill="#228B22"
+      />
+    </g>
+  </svg>
+</div>
+<div class="leaf7">
+  <svg xmlns="http://www.w3.org/2000/svg" style="vector-effect: non-scaling-stroke;" stroke="null">
+    <g stroke="null">
+      <path stroke="#f90202" d="m70,159c102,-16.597 98.862,-36 138,-35c39.138,-2 49,19.403 134,35c-2,-0.403 -62.862,0 -138,0c-72.138,-1 -135,0.597 -134,0z" id="svg_10" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="0" fill="#228B22"
+      />
+    </g>
+  </svg>
+</div>
+<div class="leaf8">
+  <svg xmlns="http://www.w3.org/2000/svg" style="vector-effect: non-scaling-stroke;" stroke="null">
+    <g stroke="null">
+      <path stroke="#f90202" d="m70,159c102,-16.597 98.862,-36 138,-35c39.138,-2 49,19.403 134,35c-2,-0.403 -62.862,0 -138,0c-72.138,-1 -135,0.597 -134,0z" id="svg_10" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="0" fill="#e3ffe3"
+      />
+    </g>
+  </svg>
+</div>
+<div id="MenuHomeLogin">
+<?php
+echo '<a href="https://covervideo.com/register.php" title="Register"><i class="fas fa-user-plus fa-2x"></i></a><br><br>';
+?>
+</div>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'> 
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<style>
+#loginheader
+{
+	font-family: lucida calligraphy, cursive;
+	font-size:35px;
+	z-index: 2;
+	position:absolute;
+	top:172px;
+	left:546px;
+	width:350px;
+	border:0px solid #324a10;
+    border-top-right-radius:250px 150px;
+    border-top-left-radius:250px 150px;
+    border-bottom-left-radius:5px 75px;
+/* Again, the ugly brilliant hack. */
+	voice-family: "\"}\"";
+	voice-family:inherit;
+}
+</style>
+<div id="loginheader">
+<?php
+echo 'Public Login';
+?>
+</div>
+<div id="MenuHomeHome">
+<?php
+echo '<a href="https://covervideo.com/" title="Home"><i class="fas fa-home fa-3x"></i></a><br><br>';
+?>
+</div>
+</div>
+</body>
+</html>
+<?php // Flush the buffered output.
+ob_end_flush();
+?>
